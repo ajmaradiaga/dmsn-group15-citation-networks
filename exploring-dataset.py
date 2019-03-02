ac8b19c20a8ee6ad05224de84cdc5793ee35d719
Antonio Maradiaga <me@ajmaradiaga.com>
2019-03-02 14:07:35 +0000

    Exploration using clean network

--- exploring-dataset.py
+++ exploring-dataset.py
@@ -4,6 +4,7 @@
 # In[1]:
 
 
+from datetime import datetime
 import os
 import re
 
@@ -12,10 +13,8 @@ from bokeh.models.widgets import DataTable, DateFormatter, TableColumn
 from bokeh.models import ColumnDataSource
 from bokeh.plotting import show
 from bokeh.plotting import figure
-from fastparquet import write
 import numpy as np
 import pandas as pd
-import snappy
 
 from helper import display_df_with_bokeh
 
@@ -23,7 +22,7 @@ from helper import display_df_with_bokeh
 # In[2]:
 
 
-get_ipython().system('head -n 10 datasets/cit-HepTh.txt')
+get_ipython().system('head -n 10 datasets/cit-HepTh-no-cycles.csv')
 
 
 # ## Initialise variables
@@ -37,92 +36,18 @@ ABSTRACTS_FOLDER = "abstracts"
 ABSTRACTS_FOLDER_PATH = f"{DATASETS_FOLDER}/{ABSTRACTS_FOLDER}/"
 
 
-# ## Processing functions
+# ## Process paper citations
+
+# ### Edges dataset
+# 
+# 
+# **NOTE:** Here we use the clean dataset that was preprocessed in the *preprocessing-dataset.ipynb* notebook
 
 # In[4]:
 
 
-def extract_text_from_abstract(text):
-    info = {}
-    fields = ['Date:', "From:", "Title:", "Authors:", "Comments:", "Subj-class:", "Journal-ref:"]
-    
-    for field in fields:
-        match = re.search(f"[\n\r].*{field}\s*([^\n\r]*)", text, re.I)
-        value = None
-        if match is not None:
-            value = match.group(1)
-        
-        info[field.replace(':', '').lower()] = value
-        
-    return info
-
-ignore_emails = ['g@c']
-ignore_tlds = ['g@c', '']
-ignore_domains = ['c', '']
-
-def domain_and_tld_from_email(email):
-    domain = None
-    tld = None
-    email = email.lower()
-    if '.' in email:
-                        
-        # Remove cases when email finishes with a period
-        if email[-1] == '.':
-            email = email[:-1]
-
-        domain = email.split('@')[1].lower().strip()
-
-        if '.ac.' in domain or '.co.' in domain or '.edu.' in domain or '.gov.' in domain or '.com.' in domain:
-            domain = ".".join(domain.split('.')[-3:])
-        else:
-            domain = ".".join(domain.split('.')[-2:])
-
-        tld  = email.split('.')[-1].lower().strip()
-
-        if tld in ignore_tlds:
-            tld = None
-
-        if domain in ignore_domains:
-            domain = None
-        
-    
-    return domain, tld
-
-# Code below from https://stackoverflow.com/a/40449726/9527459
-def explode(df, lst_cols, fill_value=''):
-    # make sure `lst_cols` is a list
-    if lst_cols and not isinstance(lst_cols, list):
-        lst_cols = [lst_cols]
-    # all columns except `lst_cols`
-    idx_cols = df.columns.difference(lst_cols)
-
-    # calculate lengths of lists
-    lens = df[lst_cols[0]].str.len()
-
-    if (lens > 0).all():
-        # ALL lists in cells aren't empty
-        return pd.DataFrame({
-            col:np.repeat(df[col].values, lens)
-            for col in idx_cols
-        }).assign(**{col:np.concatenate(df[col].values) for col in lst_cols}) \
-          .loc[:, df.columns]
-    else:
-        # at least one list in cells is empty
-        return pd.DataFrame({
-            col:np.repeat(df[col].values, lens)
-            for col in idx_cols
-        }).assign(**{col:np.concatenate(df[col].values) for col in lst_cols}) \
-          .append(df.loc[lens==0, idx_cols]).fillna(fill_value) \
-          .loc[:, df.columns]
-
-
-# ## Process paper citations
-
-# In[5]:
-
-
-with open(f"{DATASETS_FOLDER}/cit-HepTh.txt", 'r') as f:
-    df = pd.read_csv(f,sep='\t',skiprows=(0,1,2))
+with open(f"{DATASETS_FOLDER}/cit-HepTh-no-cycles.csv", 'r') as f:
+    df = pd.read_csv(f)
 
 # Dropping duplicates
 df.drop_duplicates(inplace = True)
@@ -130,23 +55,21 @@ df.drop_duplicates(inplace = True)
 # Rename columns
 df.columns = ['FromNodeId', 'ToNodeId']
 
+df.FromNodeId = df.FromNodeId.map(str)
+df.ToNodeId = df.ToNodeId.map(str)
+
+
 tlds_csv = pd.read_csv(f"{DATASETS_FOLDER}/tlds.csv", header=None, index_col=0, squeeze=True).to_dict()
 tlds_info = tlds_csv[1]
 
 
-# In[ ]:
+# In[5]:
 
 
-df.to_csv(f"{DATASETS_FOLDER}/cit-HepTh-nodup.txt", index=False)
+df.head()
 
 
 # In[6]:
-
-
-display_df_with_bokeh(df[150:250])
-
-
-# In[7]:
 
 
 # Paper that cites most papers
@@ -158,7 +81,7 @@ display_df_with_bokeh(out_degree, columns={
 })
 
 
-# In[8]:
+# In[7]:
 
 
 hist, edges = np.histogram(out_degree['ToNodeId'], bins=100, range = [0, 600])
@@ -178,7 +101,7 @@ p.quad(bottom=0, top=hist,
 show(p)
 
 
-# In[9]:
+# In[8]:
 
 
 # Paper cited the most -> Most influential
@@ -190,7 +113,7 @@ display_df_with_bokeh(in_degree, columns={
 })
 
 
-# In[10]:
+# In[9]:
 
 
 hist, edges = np.histogram(in_degree['FromNodeId'], bins=100)
@@ -210,7 +133,7 @@ p.quad(bottom=0, top=hist,
 show(p)
 
 
-# In[11]:
+# In[10]:
 
 
 degrees = pd.concat([in_degree, out_degree], axis=1, sort=False)
@@ -218,221 +141,6 @@ degrees.columns = ['out_degree', 'in_degree']
 degrees.index.name = 'paper'
 
 display_df_with_bokeh(degrees, include_index=True)
-
-
-# # Process paper abstracts
-
-# In[12]:
-
-
-abstracts_info = {}
-
-for dir_name in os.listdir(f"{ABSTRACTS_FOLDER_PATH}"):
-    try:
-        year = int(dir_name)
-        
-        for f_name in os.listdir(f"{ABSTRACTS_FOLDER_PATH}/{year}"):
-            with open(f"{ABSTRACTS_FOLDER_PATH}/{year}/{f_name}", 'r') as f:
-                abstract = f.read()
-                
-                # Parts of the abstract
-                abstract_parts = abstract.split('\\\\')
-                
-                paper_description = (abstract_parts[2] if len(abstract_parts) > 1 else "").strip()
-                
-                # Process emails
-                emails_found = re.findall(r'[\w\.-]+@[\w\.-]+', abstract)
-                
-                emails = []
-                
-                for email in emails_found:
-                    email = email.lower()
-                    
-                    if '.' in email:
-                        # Remove cases when email finishes with a period
-                        if email[-1] == '.':
-                            email = email[:-1]
-                        
-                        emails.append(email)
-                
-                
-                key = int(f_name.replace(".abs", ""))
-                
-                abstracts_info[key] = {
-                    "emails": list(set(emails)),
-#                     "tlds": list(set(tlds)),
-#                     "domains": list(set(domains)),
-                    "description": paper_description
-                }
-                
-                abstracts_info[key].update(extract_text_from_abstract(abstract))
-                
-                
-    except ValueError:
-        pass 
-
-
-# In[13]:
-
-
-papers = pd.DataFrame.from_dict(abstracts_info, orient='index')
-
-papers = pd.concat([papers, degrees], axis=1, sort=False)
-
-
-# In[14]:
-
-
-papers.head()
-
-
-# In[15]:
-
-
-more_than_one_email =  [True if len(e) == 0 else False for e in papers.emails]
-papers[more_than_one_email]
-
-
-# # Enrich Paper citations
-
-# In[16]:
-
-
-citations = df.join(papers[['emails']], on='FromNodeId')
-citations.columns = ['FromNodeId', 'ToNodeId', 'emails_from']
-
-citations = citations.join(papers[['emails']], on='ToNodeId')
-citations.columns = ['FromNodeId', 'ToNodeId', 'emails_from', 'emails_to']
-
-explode_columns = ['emails_from', 'emails_to']
-
-for ec in explode_columns:
-    citations = explode(citations, [ec])
-
-citations.drop_duplicates(inplace=True)
-    
-display_df_with_bokeh(citations.head(20))
-
-
-# In[17]:
-
-
-citations["domain_from"], citations["tld_from"] = zip(*citations["emails_from"].map(domain_and_tld_from_email))
-citations["domain_to"], citations["tld_to"] = zip(*citations["emails_to"].map(domain_and_tld_from_email))
-
-display_df_with_bokeh(citations)
-
-
-# In[18]:
-
-
-citations.head()
-
-
-# In[19]:
-
-
-# Example of papers with multiple email addresses
-citations[(citations['FromNodeId'] == 9903234) & (citations['ToNodeId'] == 9708001)]
-
-
-# In[20]:
-
-
-# Papers that cite themselves
-citations[(citations['FromNodeId'] == citations['ToNodeId'])]
-
-
-# ## TLD Aggregation
-
-# In[21]:
-
-
-# pd.Series([item for sublist in papers.tlds for item in sublist])
-
-# Flatten tlds
-tld_series = pd.concat([citations.tld_from], axis=0) 
-
-# Count different values
-tld_df = tld_series.value_counts().sort_index().rename_axis('tld').reset_index(name='count')
-
-# Add description column
-tld_df['tlds_description'] = tld_df['tld'].map(lambda x: tlds_info[x] if x in tlds_info else None)
-
-
-# In[22]:
-
-
-display_df_with_bokeh(tld_df.sort_values('count', ascending=False))
-
-
-# ## Papers in network by institution
-
-# In[23]:
-
-
-all_from = citations[['FromNodeId','domain_from']]
-all_from.columns = ['Paper', 'domain']
-
-all_to = citations[['ToNodeId','domain_to']]
-all_to.columns = ['Paper', 'domain']
-
-# Join all nodes and remove duplicates.
-# As a paper can be as a FromNode and ToNode, we will remove duplicates
-all_nodes = all_from.append(all_to).drop_duplicates()
-
-print(all_nodes.shape)
-
-# Count by domain
-domains_contributions = all_nodes.domain.value_counts().sort_index().rename_axis('domain').reset_index(name='count')
-
-# Sorting by count
-display_df_with_bokeh(domains_contributions.sort_values('count', ascending=False))
-
-
-# ## Most cited institution / lab
-
-# In[24]:
-
-
-# Only taking in consideration the domain of the ToNode
-domain_series = pd.concat([citations.domain_to], axis=0) 
-
-# Count different values
-domain_df = domain_series.value_counts().sort_index().rename_axis('domain').reset_index(name='count')
-
-# Sorting by count
-display_df_with_bokeh(domain_df.sort_values('count', ascending=False))
-
-
-# # Institutions / labs that cited the most
-
-# In[25]:
-
-
-# Flatten tlds
-domain_series_to = pd.concat([citations.domain_from], axis=0) 
-
-# Count different values
-domain_df_to = domain_series_to.value_counts().sort_index().rename_axis('domain').reset_index(name='count')
-
-# Sorting by count
-display_df_with_bokeh(domain_df_to.sort_values('count', ascending=False))
-
-
-# ## Institutions per country
-# ![alt text](visualisation/screenshots/institutions_per_country.jpeg "Title")
-
-# ## Save Dataframes
-
-# In[ ]:
-
-
-# Save dataframes
-write(f"{DATASETS_FOLDER}/parquet/paper_citations.pq", citations)
-write(f"{DATASETS_FOLDER}/parquet/papers.pq", papers)
-write(f"{DATASETS_FOLDER}/parquet/tld_aggregation.pq", tld_df)
-write(f"{DATASETS_FOLDER}/parquet/domain_aggregation.pq", domain_df)
 
 
 # In[ ]:
